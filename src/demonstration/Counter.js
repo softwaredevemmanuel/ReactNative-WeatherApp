b18a7f94691f0ae05ad5ad111833194b2b864172
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-02 19:30:13 +0100

    useEffect

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -1,8 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
 export default Counter = () => {
     const [count, setCount] = useState(0)
+
+    const [newCount, setNewCount] = useState(0)
+
+
+    
+    useEffect(()=>{
+        console.log(` Count Changed`)
+        return () => {
+            console.log('useeffect cleanup')
+        }
+    }, [newCount])
+
     return (
         <View style= {styles.container}>
             <Text style= {styles.title}>{`count: ${count}`}</Text>
@@ -18,6 +30,20 @@ export default Counter = () => {
                 title={'Decrease the count'}
                 onPress={() => { 
                     setCount(count - 1)
+                    }} 
+            />
+               <Button
+                color={'green'}
+                title={'Increase the count'}
+                onPress={() => { 
+                    setNewCount(count + 1)
+                    }} 
+            />
+            <Button
+                color={'red'}
+                title={'Decrease the count'}
+                onPress={() => { 
+                    setNewCount(count - 1)
                     }} 
             />
         </View>
