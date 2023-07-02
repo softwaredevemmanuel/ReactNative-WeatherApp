e289221f9d7914902482d777de5e1a926d3eb35b
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-03 00:40:48 +0100

    seperate useState, useEffect in useWeather.js

--- App.js
+++ App.js
@@ -5,48 +5,12 @@ import Tabs from './src/components/Tabs';
 import Counter from './src/demonstration/Counter';
 import * as Location from 'expo-location';
 import { WEATHER_API_KEY } from "@env";
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+import { useGetWeather } from './src/hooks/useGetWeather';
 
 export default function App() {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
 
-
-  const fetchWeatherData = async () => {
-    try{
-      const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-      const data = await response.json()
-      setWeather(data)
-
-    }catch(error){
-      setError('Could not fetch weather')
-    } finally{
-      setLoading(false)
-    }
-    
-  }
-
-  useEffect(() => {
-    (async()=> {
-      let {status} = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted'){
-        setError('Permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather){
-    console.log(weather)
-  }
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
   if (loading){
     return(
