c253e2d5f1465d555df3194f2ed85e8835a64658
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-02 19:23:21 +0100

    useState

---
+++ src/demonstration/Counter.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+export default Counter = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <View style= {styles.container}>
+            <Text style= {styles.title}>{`count: ${count}`}</Text>
+            <Button
+                color={'green'}
+                title={'Increase the count'}
+                onPress={() => { 
+                    setCount(count + 1)
+                    }} 
+            />
+            <Button
+                color={'red'}
+                title={'Decrease the count'}
+                onPress={() => { 
+                    setCount(count - 1)
+                    }} 
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'orange'
+    },
+    title: {
+        alignSelf: 'center',
+        fontSize: 25,
+        marginTop: 25
+    }
+})
